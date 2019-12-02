73cd2881374abe53ba96114a5fde265f0c23cf4d
Vladislav Kalugin <kalugin.vlad@gmail.com>
2019-12-02 11:51:50 +0300

    update

--- RCA/src/com/company/Encoder.java
+++ RCA/src/com/company/Encoder.java
@@ -1,4 +1,16 @@
 package com.company;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Encoder {
+    private BigInteger p, q, n, fi, e, d;
+    byte[] encode(byte[] m) {
+        byte[] dest = new byte[n.bitLength()*(m.length / (n.bitLength() - 1))];
+        for(int i = 0; i < m.length; i += (n.bitLength() - 1)) {
+            var t = Arrays.copyOfRange(m, i, Integer.max(i + n.bitLength() - 1, m.length));
+        }
+    }
 }
